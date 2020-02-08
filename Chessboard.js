b0460f1161f91b3339f36dec5079d147122f7b96
Emma Saroyan <emma.saroyan@gmail.com>
2020-02-08 21:19:55 +0400

    Exercise 3

---
+++ Chessboard.js
@@ -0,0 +1,13 @@
+let chessBoard = "";
+let size = 8;
+
+for (let i = 0; i <= size; i++) {
+  for (let j = 0; j<= size; j++) {
+
+    if ((i + j) % 2 == 0) chessBoard += " ";
+    else chessBoard += "#";
+
+  }
+  chessBoard += "\n";
+}
+console.log(chessBoard);
